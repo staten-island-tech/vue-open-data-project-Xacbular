9006286e3adc7b53ecc2e7717bbe5623a039241c
Xacbular <jacobwu2592@gmail.com>
2026-03-27 14:01:13 -0400

    bop

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import ArrestDetails from '@/views/ArrestDetails.vue'
+import Statistics from '@/views/Statistics.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/statistics',
+      name: 'statistics',
+      component: Statistics,
     },
     {
       path: '/arrest/:id',
